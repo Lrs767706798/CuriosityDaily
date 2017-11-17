6561989d4199eb2760ebadf0fc99963367ebcf03
Lrs767706798 <17854228612@163.com>
2017-11-17 10:52:11 +0800

    over

---
+++ components/article/article.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="mobile articles show box">
+		<xheader></xheader>
+		<xmain></xmain>
+		<xbg></xbg>
+		<!-- <router-view></router-view> -->
+	</div>
+</template>
+
+<script>
+	import xheader from './header.vue'
+	import xmain from './main.vue'
+	import xbg from './bg.vue'
+	export default {
+		components: {
+			xheader,
+			xmain,
+			xbg
+		}
+	}
+</script>
+
+<style scoped>
+</style>
